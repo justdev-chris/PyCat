7e9743e212c0fcf9a0a9a82fdb4d61c071bf8d30
Chris <morenochristopher580@gmail.com>
2026-01-25 21:34:13 -0600

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,9 @@
+"""
+PyCat - A cat-themed programming language
+"""
+
+from .keywords import CAT_TO_PYTHON, translate
+from .main import run
+
+__version__ = "1.0.0"
+__all__ = ['CAT_TO_PYTHON', 'translate', 'run']
